b8de2ba33f6f25ca7d228748c95be467bef23625
Josh Hawks <snl95@txstate.edu>
2023-04-12 15:41:33 -0500

    update PrimaryUser model validations and methods

--- app/models/primary_user.rb
+++ app/models/primary_user.rb
@@ -1,2 +1,19 @@
 class PrimaryUser < ApplicationRecord
+  validates :first_name, :last_name, :email, presence: true
+  validates :email,
+            uniqueness: true,
+            format: {
+              with: URI::MailTo::EMAIL_REGEXP,
+            }
+  validates :access_code, uniqueness: true
+  validates :unique_url_hash, uniqueness: true
+
+  before_create :generate_unique_link_and_access_code
+
+  private
+
+  def generate_unique_link_and_access_code
+    self.access_code = SecureRandom.hex(10)
+    self.unique_url_hash = SecureRandom.uuid
+  end
 end
