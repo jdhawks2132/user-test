9906ecf40871a096d48fe2d13be9033f2131cc4e
Josh Hawks <snl95@txstate.edu>
2023-04-12 15:51:27 -0500

    configure sessions

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,6 +22,10 @@ module UserTest
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+                          key: '_cycleone_session',
+                          expire_after: 14.days
     config.api_only = true
   end
 end
